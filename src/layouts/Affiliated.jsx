80f620288098bb2289f4ad612aced0c18e5883b3
Someshwar <someshwarudrakshya@gmail.com>
2025-08-28 11:08:46 +0545

    Update: changes

--- src/layouts/Affiliated.jsx
+++ src/layouts/Affiliated.jsx
@@ -1,35 +1,18 @@
-import React from "react";
-import Nepal from '../assets/partner/Ne.png'
-import National from '../assets/partner/Na.jpg'
-import Jyoti from '../assets/partner/Jyoti.png'
-import Meta from '../assets/partner/Meta.png'
+import National from "../assets/affiliated/1.avif";
+import NEFSCUM from "../assets/affiliated/2.avif";
+import BTS from "../assets/affiliated/3.avif";
+import DEV from "../assets/affiliated/4.avif";
 
 function Partner() {
   return (
     <section className="partners">
       <div className="container">
-        <h3>Trusted by leading insurance providers:</h3>
+        <h3>Trusted by Affiliated :</h3>
         <div className="partner-logos">
-          <img
-            src={Nepal}
-            alt="Nepal Life Insurance"
-            loading="lazy"
-          />
-          <img
-            src={National}
-            alt="National Life Insurance"
-            loading="lazy"
-          />
-          <img
-            src={Jyoti}
-            alt="Jyoti Life Insurance"
-            loading="lazy"
-          />
-          <img
-            src={Meta}
-            alt="MetLife Insurance"
-            loading="lazy"
-          />
+          <img src={National} alt="National Cooperative Bank" loading="lazy" />
+          <img src={NEFSCUM} alt="NEFSCUM" loading="lazy" />
+          <img src={BTS} alt="Bachat Tatha Rin Sangh" loading="lazy" />
+          <img src={DEV} alt="Site Developer Company" loading="lazy" />
         </div>
       </div>
     </section>
